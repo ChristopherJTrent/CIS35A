3371fdd894fd104361739eae4cc2b7afac21c200
Christopher Aleister Trent <christopherjtrent@business-cs.deanza.ad.fhda.edu>
2019-04-24 14:42:35 -0700

    Completed lab 1

--- Lab1/SumDigits.java
+++ Lab1/SumDigits.java
@@ -1,45 +1,32 @@
 import java.util.ArrayList;
 
 public class SumDigits {
-	public static int[] getDigits(int in) {		
-		int length = Math.abs((int)(Math.log10(in)+1)); //(Log base 10 of input) + 1 gives the total number of digits in a number, absolute value prevents negative array length.
-		int[] out = new int[length+1]; //Define an array that can comfortably hold the digits of the number.
-		for(int i = 0; i<length; i++){ //loop across the number's digits.
-			out[i] = in%10; //Extract the Ith digit
-			in /= 10; //and remove it.
-		}
-		return out; //return the int array of digits.
-	}
-	public static int sumDigits(int[] in) {
-		int answer=0;
-		for(int i : in){ //enhanced for loop. "for int i in in, do"
-			answer+=i; //add i to the answer.
-		}
-		return answer; // pass it back.
-	}
-	public static void test(){
-		System.out.println("12345: " + sumDigits(getDigits(12345))); //test 1 through 5
-		System.out.println("98765: " + sumDigits(getDigits(98765))); //test 9 through 5
-		System.out.println("2468: " + sumDigits(getDigits(2468))); //all single digit even numbers
-		System.out.println("13579: " + sumDigits(getDigits(13579))); //all single digit odd numbers
-		System.out.println(Integer.MAX_VALUE +": " + sumDigits(getDigits(Integer.MAX_VALUE))); //2^31-1
-	}
 	public static void main(String[] args) {
-		java.util.scanner s = new Scanner(System.in);
+		//prep code
+		java.util.Scanner s = new java.util.Scanner(System.in); //Define a Scanner
+		//business logic
 		while(true) {
-			System.out.println("Input a number between 1 and " + Integer.MAX_VALUE);
-			int input = s.nextInt();
-			int length = Math.abs((int)(Math.log10(input)+1));
-			int[] out = new int[length];
-			for(int i = 0; i<length; i++){
-				out[i] = input%10;
-				input /= 10;
+			//I/O
+			System.out.println("Input a number between 1 and " + Integer.MAX_VALUE + " or 0 to exit"); //prompt the user
+			int input = s.nextInt(); //grab their input
+			//Manual Termination
+			if(input == 0) break; //Exit if 0.
+			//Integer to IntArray
+			int length = Math.abs((int)(Math.log10(input)+1)); //find the number of digits, log base 10 gives 1 less than that.
+			int[] out = new int[length]; //Dynamically Allocate an integer array.
+			for(int i = 0; i<length; i++){ //Loop across that array
+				out[i] = input%10; //set each index to the value of that digit.
+				input /= 10; //Remove the ith digit from the array.
 			}
+			//IntArray to Sum
 			int answer = 0;
-			for(int i : input) {
+			for(int i : out) { //Enhanced for loop across the array.
 				answer += i;
 			}
-			System.out.println("The sum of the digits of " + input + "is " + answer);
+			//output
+			System.out.println("The sum of those digits is " + answer); //output the answer to the console.
 		}
+		//resource management
+		s.close(); //Prevent resource leaks.
 	}
 }
