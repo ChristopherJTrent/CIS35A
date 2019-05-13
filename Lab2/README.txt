d73ccc0bbf1eff5c322e03d3744f2bb55c54a424
Christopher Aleister Trent <christopherjtrent@business-cs.deanza.ad.fhda.edu>
2019-05-13 12:09:55 -0700

    Updated code to remove array usage.

--- Lab2/README.txt
+++ Lab2/README.txt
@@ -1,11 +1,7 @@
 The included PNG file is a flow-chart outlining the control-flow of the included Java Program.
 
 
-The file Assigment2.txt is a PlantUML source file, which can be compiled into a PNG file identical to the one that is included.
+The file VendingMachine.puml is a PlantUML source file, which can be converted using PlantUML into a PNG file identical to the one that is included.
 
-	PlantUML source code is essentially weaponized pseudocode that can be turned into a flowchart.
-	
-As such, I hope that either the included image, or the PlantUML source used to create it 
-	is sufficient to
- fulfill my pseudocode/Flowchart requirement.
-
+	PlantUML source code is essentially weaponized pseudocode that can be turned into a flowchart. As such, I hope that either the included image, or the PlantUML source used to create it 
+	is sufficient to fulfill my pseudocode/Flowchart requirement.
--- Lab2/VendingMachine.java
+++ Lab2/VendingMachine.java
@@ -1,61 +1,100 @@
-public class VendingMachine {
+public class VendingMachine2 {
 	public static void main(String[] args) {
 		//Initialization
 		java.util.Scanner input = new java.util.Scanner(System.in);
 		boolean exit = false;
-		int[] quantities = { 3, 5, 8 };
+		int chipQuantity = 3;
+		int cookieQuantity = 5;
+		int candyQuantity = 8;
 		int total = 0;
 		int quarters = 0;
 		int dimes = 0;
 		int nickels = 0;
-		final int[] prices = { 125, 85, 95 }; // all internal values are in integer cents, to avoid rounding errors
-		final String[] items = { "Potato Chips", "Cookies", "Candy" };
-		final int[] coins = { 25, 10, 5 };
 		//End Initialization
 		//User Input
 		do {
-		System.out.println("\nPlease Insert your change");
-		System.out.print("Quarters: ");
-		quarters = input.nextInt();
-		System.out.print("Dimes: ");
-		dimes = input.nextInt();
-		System.out.print("Nickels: ");
-		nickels = input.nextInt();
-		total += ((quarters * coins[0]) + (dimes * coins[1]) + (nickels * coins[2])); //grab
-			System.out.printf(
-					"Vending Machine \nCredit: $%4.2f \nSelect an option by inputting its number: \n1) Potato Chips $1.25, %d left\n2) Cookies $0.85 %d left \n3) Candy $0.95 %d left\n0) Leave without buying anything \n",
-					total / 100.0, quantities[0],quantities[1],quantities[2]);
+			System.out.println("\nPlease Insert your change");
+			System.out.print("Quarters: ");
+			quarters = input.nextInt();
+			System.out.print("Dimes: ");
+			dimes = input.nextInt();
+			System.out.print("Nickels: ");
+			nickels = input.nextInt();
+			total += ((quarters * 25) + (dimes * 10) + (nickels * 5)); //grab
+			System.out.printf("Vending Machine \nCredit: $%4.2f \nSelect an option by inputting its number: \n1) Potato Chips $1.25, %d left\n2) Cookies $0.85 %d left \n3) Candy $0.95 %d left\n0) Leave without buying anything \n",
+				total / 100.0, chipQuantity,cookieQuantity,candyQuantity);
 			int in = input.nextInt();
 			//End User Input
 			//Business Logic
-			if (in == 0) {
-				exit = true;
-			} else if (in == 999){
-				quantities[0] = 1;
-				quantities[1] = 2;
-				quantities[2] = 3;
-				System.out.println("Testing values loaded, quantities set to 1, 2, and 3.");
-			} else if (in > 3){  //Highest Item number is 3, all higher input can be discarded as invalid.
-				System.out.printf("There is no item with that number. You have %.2f remaining\n", total/100.0); //lossy float display only occurs in human-readable settings.
-			}else {
-				in--;
-				if(quantities[in] == 0){ //Check if the item is sold out.
-					System.out.println("That item is sold out, please select a different item.");
+			int price = 0;
+			String selection = "";
+			switch(in) {
+				case 0: exit=true;
+					break;
+				case 1: 
+					price = 125;
+					selection = "Chips";
+					break;
+				case 2: 
+					price = 85;
+					selection = "Cookies";
+					break;
+				case 3: 
+					price = 95;
+					selection = "Candy";
+					break;
+				default:
+					System.out.printf("There is no item with number %d, please make a different selection.", in);			
+			}
+			if(!exit){
+				if(price <= total){
+					switch(in){
+						case 1: 
+							if(chipQuantity > 0){
+								chipQuantity--;
+								total -= price;
+								System.out.printf("Here are your chips. You have $%.2f remaining.", total/100f);
+								break;
+							} else {
+								System.out.println("That item is sold out. please select another.");
+								break;
+							}
+						case 2:
+							if(cookieQuantity > 0){
+								cookieQuantity--;
+								total -= price;
+								System.out.printf("Here are your cookies. You have $%.2f remaining.", total/100f);
+								break;
+							}else {
+								System.out.println("That item is sold out. please select another.");
+								break;
+							}
+						case 3:
+							if (candyQuantity > 0) {
+								candyQuantity--;
+								total -= price;
+								System.out.printf("Here's your candy. You have $%.2f remaining.",total/100f);
+								break;
+							} else {
+								System.out.println("That item is sold out, please select another.");
+								break;
+							}
+						default:
+							System.out.println("Unexpected error state reached.");
+							break;
+					}
+				} else {
+					System.out.printf("You have insufficient funds to purchase %s, please select a different item. You have %.2f credit.", selection, total/100f);
 				}
-				else if (prices[in] == total) { //check if exact change was given
-					System.out.printf("Here's your %s!\nYou paid with exact change.\n ", items[in]);
-					total = 0;
-					quantities[in]--;
-				} else if (prices[in] < total) { //otherwise, check if sufficient payment was given.
-					total -= prices[in];
-					System.out.printf("Here's your %s!\nYou have %.2f remaining\n", items[in], total / 100.0);
-					quantities[in]--;
-				} else { //if neither, insufficient payment was supplied, inform the user.
-					System.out.println(
-							"You have insufficient credit to buy this item, please insert more coins, or choose a less expensive item.");
+			} else {
+				if (total > 0){
+					System.out.printf("Have a nice day, your change is %.2f\n",total/100f);
+				} else {
+					System.out.println("Have a nice day.");
 				}
 			}
 		} while (!exit); //flag value.
+	input.close();
 	}
 }
 //End Business Logic
